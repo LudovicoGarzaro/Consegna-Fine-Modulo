73d928958ced88d1d5d915143c27e53afbb04c12
LudovicoGarzaro <ludovico.garzaro@gmail.com>
2023-02-15 17:59:45 +0100

    Modifica Player, aggiunta materials e proseguimento programmazione

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,25 +5,92 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] LayerMask groundMask;
 
+    public int score = 0;
+
+    bool isGrounded = false;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        //if (rb != null)
+        //{
+        //    Debug.Log(rb.mass);
+        //}
+        //else 
+        //{
+        //    Debug.LogError("Rigidbody not found");
+        //}
+
+
     }
 
     
     void Update()
     {
         float xMove = Input.GetAxis("Horizontal");
-        float yMove = Input.GetAxis("Vertical");
+        float zMove = Input.GetAxis("Vertical");
+        
+        //Costruisco il vettore movimento
+        Vector3 velocity = (Vector3.forward * zMove + Vector3.right * xMove).normalized * speed; //* Time.deltaTime ;
+        velocity.y = rb.velocity.y;
+        //transform.position = transform.position + playerMovement;
 
-        Vector3 playerMovementy = Vector3.forward * yMove * speed * Time.deltaTime;
-        Vector3 playerMovementx = Vector3.right * xMove * speed * Time.deltaTime;
-       
-        transform.position += playerMovementx;
-        transform.position += playerMovementy;
+        //Applico la traslazione
+        //transform.Translate(playerMovement, Space.World);
+
+        
+        velocity.y = rb.velocity.y;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            //velocity.y += Mathf.Sqrt(jumpForce * -2f * (-9.81f));
+        }
+
+        rb.velocity = velocity;
+
+        Debug.DrawRay(transform.position, -transform.up * 10, Color.red);
+
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, -transform.up, out hit, 10, groundMask))
+        {
+            Debug.Log("Colpito");
+        }
+
+        //Physics.SphereCast
+        //Physics.OverlapSphere
     }
 
+   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Collectible"))
+        {
+            score++;
+            Debug.Log(score);
+            Destroy(other.gameObject);
+        }
+
+        if (other.transform.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
 
 }
+
